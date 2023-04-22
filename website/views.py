922cb896e1c50710c99e629b7bb9cbc6f6cbe03f
danndongi02 <ianndunguh@gmail.com>
2023-04-22 21:01:56 +0300

    Added logout_user

--- website/views.py
+++ website/views.py
@@ -26,9 +26,7 @@ def home(request):
         return render(request, 'home.html', {})
 
 
-def login_user(request):
-    pass
-
-
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect('home')
