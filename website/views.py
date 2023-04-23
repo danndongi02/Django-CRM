165a2a5260f88a237af69475153931bc024f074b
danndongi02 <ianndunguh@gmail.com>
2023-04-23 04:37:46 +0300

    Added register_user function

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
-# Create your views here.
 
-
+# Home view
 def home(request):
     # Check to see if user is logging in
     if request.method == "POST":
@@ -25,8 +25,33 @@ def home(request):
     else:
         return render(request, 'home.html', {})
 
+# Logout the user
+
 
 def logout_user(request):
     logout(request)
     messages.success(request, "Logged out successfully!")
     return redirect('home')
+
+
+# Register a new user
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # Authentication and log in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Registration successful! Welcome!")
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
