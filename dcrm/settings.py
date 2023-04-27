6dfd5042f4f6febb46966624b8f6e0bb975c8a77
danndongi02 <ianndunguh@gmail.com>
2023-04-27 08:57:59 +0300

    Removed secret key"

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t)$ab%cnsrn^!xd**rho3w8q!-9$@ea0n3^pn289llepody!b8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
